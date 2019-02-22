bb8be3e9154e83c41060f9a21a9943d9377fe40e
DESKTOP-94V8FHF\Huy <nguyenhuysn@gmail.com>
2019-02-22 14:44:36 +0700

    add file player bullet

---
+++ Archery Adventure/Archery Adventure/Assets/Script/PlayerBullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : Bullet {
+
+	protected override void LateUpdate()
+	{
+		Vector3 screenPoint = Camera.main.WorldToViewportPoint(transform.position);
+		if(screenPoint.x > 1 || screenPoint.y < 0)
+		{
+			Destroy(this.gameObject);
+		
+			GameController.Instance.EnemyAttack ();
+		}     
+	}
+}
